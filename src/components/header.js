e9ce586266902863cc4bd900fb18600d894c4788
Kittaru87 <sophbell87@gmail.com>
2020-07-30 15:02:39 +0100

    another styling method - modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from "react"
+import Styles from "./header.module.css"
 
 export default function Header(props) {
-  return <h1>{props.headerText}</h1>
+  return <h1 className={Styles.underline}>{props.headerText}</h1>
 }
