4ba4ee0233add4fccc19fee053e203fd76c8063a
Kittaru87 <sophbell87@gmail.com>
2020-08-03 09:26:36 +0100

    creating a list of markdown file pages in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
 import { graphql } from "gatsby"
 
 export default function Home({ data }) {
   return (
     <Layout>
-      <div>
-        <h1>Welcome to {data.site.siteMetadata.title}</h1>
+      {/* <div>
+        <h1>Welcome to {data.siteMetadata.title}</h1>
         <p> What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
         <p>
@@ -14,23 +16,53 @@ export default function Home({ data }) {
           This is a fake site to practice using Gatsby. Isn't it wonderful!
         </p>
         <p>Now to try some GraphQL Queries</p>
-        <h3>Amazing Pandas Eating Things</h3>
-        <div>
-          <img
-            src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
-            alt="Group of pandas eating bamboo"
-          />
-        </div>
       </div>
+      <div> */}
+      <h2
+        css={css`
+          display: inline-block;
+          border-bottom: 1px solid;
+        `}
+      >
+        Amazing Pandas Eating Things
+      </h2>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3
+            css={css`
+              margin-bottom: ${rhythm(1 / 4)};
+            `}
+          >
+            {node.frontmatter.title}{" "}
+            <span
+              css={css`
+                color: #bbb;
+              `}
+            >
+              — {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+      {/* </div> */}
     </Layout>
   )
 }
-
 export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
       }
     }
   }
